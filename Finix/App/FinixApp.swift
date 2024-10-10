00bd8451cce6a649870c499fa155115b41a965d9
Valerie N. Prinz <vprinz.me@yandex.ru>
2024-10-10 11:05:43 +0100

    - create Wallter model

--- Finix/App/FinixApp.swift
+++ Finix/App/FinixApp.swift
@@ -12,7 +12,7 @@ import SwiftData
 struct FinixApp: App {
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
-            Item.self,
+            Wallet.self,
         ])
         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
 
