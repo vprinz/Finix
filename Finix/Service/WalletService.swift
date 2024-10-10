58546f10f0626bd67a823300518440a4b928e910
Valerie N. Prinz <vprinz.me@yandex.ru>
2024-10-10 11:29:05 +0100

    - add WalletService

---
+++ Finix/Service/WalletService.swift
@@ -0,0 +1,27 @@
+//
+//  WalletService.swift
+//  Finix
+//
+//  Created by Valerie N. Prinz on 10/10/2024.
+//
+
+import Foundation
+import SwiftData
+
+
+
+struct WalletService {
+    let modelContext: ModelContext
+    
+    func create(name: String, currency: String) throws -> Wallet {
+        let wallet = Wallet(name: name, currency: currency)
+        modelContext.insert(wallet)
+        try modelContext.save()
+        return wallet
+    }
+    
+    func delete(_ wallet: Wallet) throws {
+        modelContext.delete(wallet)
+        try modelContext.save()
+    }
+}
