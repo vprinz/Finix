672d878ff111e74fa4d94448ec7c24d421b18487
Valerie N. Prinz <vprinz.me@yandex.ru>
2024-10-15 14:30:41 +0100

    - remove useless errors

--- Finix/Entity/Error/NetworkError.swift
+++ Finix/Entity/Error/NetworkError.swift
@@ -4,23 +4,17 @@ import Foundation
 
 enum NetworkError: AppError {
     case badResponse
-    case decodingError // TODO: Remove?
     case httpStatusCode(code: Int)
     case invalidUrl
-    case unknown // TODO: Remove?
     
     var description: String {
         switch self {
         case .badResponse:
-            "Bad response"
-        case .decodingError:
-            "Error while decoding response"
+            "Bad Response."
         case .httpStatusCode(let code):
-            "HTTP Error \(code)"
+            "HTTP Error \(code)."
         case .invalidUrl:
-            "URL is invalid"
-        case .unknown:
-            "An unknown error occured"
+            "URL is invalid."
         }
     }
 }
