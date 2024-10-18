ace5eb999c207ced130849ff3905dfdade1c5be5
Valerie <valerypavlikov@yandex.ru>
2024-10-18 10:58:38 +0100

    - add update() method to Wallet service

--- Finix/Service/EntityService/WalletService.swift
+++ Finix/Service/EntityService/WalletService.swift
@@ -11,4 +11,23 @@ class WalletService: EntityService<Wallet> {
         try modelContext.save()
         return wallet
     }
+    
+    func update(_ wallet: Wallet, name: String?, currency: Currency?) throws -> (Wallet, Bool) {
+        var isUpdated = false
+        
+        if let name = name, wallet.name != name {
+            wallet.name = name
+            isUpdated = true
+        }
+        
+        if let curency = currency, wallet.currency != curency {
+            wallet.currency = curency
+            isUpdated = true
+        }
+        
+        if isUpdated {
+            try modelContext.save()
+        }
+        return (wallet, isUpdated)
+    }
 }
