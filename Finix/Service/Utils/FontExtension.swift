ed1680c99048a3e1a375217ada4abf1ae3c749f9
Valerie <valerypavlikov@yandex.ru>
2024-10-29 10:51:28 +0000

    - add Colors

---
+++ Finix/Service/Utils/FontExtension.swift
@@ -0,0 +1,34 @@
+// Created by Valerie N. Prinz on 29/10/2024.
+
+import SwiftUI
+
+extension Font {
+    static var titleLargeSemibold = Font.system(size: 48, weight: .semibold)
+    static var titleMedium = Font.system(size: 24, weight: .medium)
+    static var titleSmall = Font.system(size: 20, weight: .medium)
+    static var subtitleSemibold = Font.system(size: 17, weight: .medium)
+    
+    static var buttonLargeRegular = Font.system(size: 17, weight: .regular)
+    static var buttonSemibold = Font.system(size: 16, weight: .semibold)
+    static var buttonMedium = Font.system(size: 16, weight: .medium)
+    
+    static var bodyTextTallRegular = Font.system(size: 16, weight: .regular)
+    static var bodyTextMiddleRegular = Font.system(size: 16, weight: .regular)
+    static var bodyTextNarrowRegular = Font.system(size: 16, weight: .regular)
+    
+    static var captionNarrowMedium = Font.system(size: 14, weight: .medium)
+    static var captionTallRegular = Font.system(size: 14, weight: .regular)
+    static var captionSemiTallRegular = Font.system(size: 14, weight: .regular)
+    static var captionRegular = Font.system(size: 14, weight: .regular)
+    static var CaptionNarrowRegular = Font.system(size: 14, weight: .regular)
+    
+    static var helperTextMedium = Font.system(size: 13, weight: .medium)
+    static var helperTextRegular = Font.system(size: 13, weight: .regular)
+    
+    static var smallTextMedium = Font.system(size: 12, weight: .medium)
+    static var smallTextTallRegular = Font.system(size: 12, weight: .regular)
+    static var smallTextRegular = Font.system(size: 12, weight: .regular)
+    
+    static var microTextMedium = Font.system(size: 10, weight: .medium)
+    static var microTextRegular = Font.system(size: 10, weight: .regular)
+}
