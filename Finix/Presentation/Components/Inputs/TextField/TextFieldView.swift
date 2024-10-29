7bfc2f0d93deb2fa1b5041057341f10b8530326f
Valerie <valerypavlikov@yandex.ru>
2024-10-29 09:31:34 +0000

    - add ButtonConfiguration to TextFieldViewModel

--- Finix/Presentation/Components/Inputs/TextField/TextFieldView.swift
+++ Finix/Presentation/Components/Inputs/TextField/TextFieldView.swift
@@ -21,6 +21,20 @@ struct TextFieldView: View {
                     viewModel.state = newValue ? .focused : .inactive
                 }
             }
+            
+            if let buttonConfig = viewModel.buttonConfig {
+                Button {
+                    buttonConfig.action()
+                } label: {
+                    Image(buttonConfig.imageName)
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 24, height: 24)
+                        .foregroundStyle(Color.customPrimary)
+                        .padding(12)
+                }
+            }
+
         }
         .frame(height: 48)
         .background(viewModel.backgroundColor)
@@ -36,6 +50,16 @@ struct TextFieldView: View {
 #Preview {
     @Previewable @State var tf2: String = ""
     var tf1: String = ""
+    
+    let buttonConfig1 = TextFieldViewModel.ButtonConfiguration(
+        imageName: "clear-circle") {
+            print("Button Config 1")
+        }
+    let buttonConfig2 = TextFieldViewModel.ButtonConfiguration(
+        imageName: "pencil") {
+            print("Button Config 2")
+        }
+
     VStack {
         TextFieldView(viewModel: TextFieldViewModel(
             value: .init(get: {
@@ -44,22 +68,15 @@ struct TextFieldView: View {
                 tf1 = newValue
             }),
             placeholder: "Enter text",
-            disabled: false
+            disabled: false,
+            buttonConfig: buttonConfig1
         ))
         TextFieldView(viewModel: TextFieldViewModel(
             value: $tf2,
             placeholder: "Enter text",
             style: .gray,
-            disabled: false
-        ))
-        TextFieldView(viewModel: TextFieldViewModel(
-            value: .init(get: {
-                ""
-            }, set: { _ in
-                
-            }),
-            placeholder: "Enter text",
-            disabled: true
+            disabled: false,
+            buttonConfig: buttonConfig2
         ))
         TextFieldView(viewModel: TextFieldViewModel(
             value: .init(get: {
