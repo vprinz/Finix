6f79d0a34f93982c9d62ff35ac037752b694408f
Valerie <valerypavlikov@yandex.ru>
2024-10-31 09:14:06 +0000

    - create SmallWalletCardView

---
+++ Finix/Presentation/Components/WalletCard/SmallWalletCardView.swift
@@ -0,0 +1,70 @@
+// Created by Valerie N. Prinz on 31/10/2024.
+
+import SwiftUI
+
+extension SmallWalletCardView {
+    struct Model: Identifiable {
+        let id: UUID
+        let currencyIsoCode: String
+        let style: Color
+        
+        init(
+            id: UUID = UUID(),
+            currencyIsoCode: String,
+            style: Color
+        ) {
+            self.id = id
+            self.currencyIsoCode = currencyIsoCode
+            self.style = style
+        }
+    }
+}
+
+struct SmallWalletCardView: View {
+    let model: Model
+    
+    var body: some View {
+        HStack {
+            Spacer()
+            Text(model.currencyIsoCode)
+                .font(.titleSmall)
+                .foregroundStyle(model.style)
+                .padding(.trailing, 6)
+                .padding(.top)
+        }
+        .frame(height: 40)
+        .background(Color.foreground)
+        .clipShape(.rect(cornerRadius: 6))
+        .overlay(
+          RoundedRectangle(cornerRadius: 6)
+            .inset(by: 0.25)
+            .stroke(model.style, lineWidth: 0.5)
+        )
+    }
+}
+
+#Preview {
+    let models: [SmallWalletCardView.Model] = [
+        SmallWalletCardView.Model(
+            currencyIsoCode: "USD",
+            style: Color.lavender
+        ),
+        SmallWalletCardView.Model(
+            currencyIsoCode: "EUR",
+            style: Color.ocean
+        ),
+        SmallWalletCardView.Model(
+            currencyIsoCode: "RUB",
+            style: Color.rose
+        )
+    ]
+    HStack {
+        ForEach(models) { model in
+            SmallWalletCardView(model: model)
+                .frame(width: 60)
+        }
+        .scrollContentBackground(.hidden)
+    }
+    .frame(width: 400, height: 400)
+    .background(Color.figmaBackground)
+}
