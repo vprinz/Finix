665df3739e69e84069dd0ab60ffcd8ddb5c1f7f2
Valerie <valerypavlikov@yandex.ru>
2024-11-08 12:56:05 +0000

    - create EmptyStateView

--- Finix/Presentation/Components/EmptyStates/EmptyStateView.swift
+++ Finix/Presentation/Components/EmptyStates/EmptyStateView.swift
@@ -14,7 +14,7 @@ extension EmptyStateView {
             case emptyLimit
         }
         
-        let state: State = .emptyTransaction
+        var state: State = .emptyTransaction
         
         var imageName: String {
             switch state {
@@ -39,14 +39,12 @@ extension EmptyStateView {
             case .emptyTransaction:
                 return .init(
                     text: String(localized: "addTransactionBtnText")) {
-                        // DEBUG
-                        print("Transaction here")
+                        // Add logic here
                     }
             case .emptyLimit:
                 return .init(
-                    text: "setLimitBtnText") {
-                        // DEBUG
-                        print("Limit here")
+                    text: String(localized: "setLimitBtnText")) {
+                        // Add logic here
                     }
             }
         }
@@ -54,11 +52,67 @@ extension EmptyStateView {
 }
 
 struct EmptyStateView: View {
+    let model: Model
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Image(model.imageName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(height: 105)
+                .padding(.trailing, 30)
+            VStack(alignment: .leading) {
+                Text(model.text)
+                    .font(.smallTextTallRegular)
+                    .foregroundStyle(Color.textSecondary)
+                    .padding(.bottom, 22)
+                Button {
+                    model.buttonConfig.action()
+                } label: {
+                    HStack(alignment: .center) {
+                        Text(model.buttonConfig.text)
+                            .font(.bodyTextNarrowRegular)
+                            .foregroundStyle(Color.customPrimary)
+                    }
+                    .padding(.horizontal, 16)
+                    .padding(.vertical, 8)
+                    .frame(maxWidth: .infinity)
+                    .cornerRadius(10)
+                    .overlay(
+                      RoundedRectangle(cornerRadius: 10)
+                        .inset(by: 0.5)
+                        .stroke(Color.customPrimary)
+                    )
+                }
+            }
+            .frame(maxWidth: UIScreen.main.bounds.width * 0.5)
+        }
+        .padding(16)
+        .frame(minHeight: 152)
+        .background(Color.white)
+        .cornerRadius(12)
+        .overlay(
+          RoundedRectangle(cornerRadius: 12)
+            .inset(by: 0.25)
+            .stroke(Color.borderStroke, lineWidth: 0.5)
+        )
     }
 }
 
 #Preview {
-    EmptyStateView()
+    let model1: EmptyStateView.Model = EmptyStateView.Model(
+        state: .emptyTransaction
+    )
+    let model2: EmptyStateView.Model = EmptyStateView.Model(
+        state: .emptyLimit
+    )
+    
+    VStack {
+        EmptyStateView(model: model1)
+            .frame(maxWidth: 360)
+        EmptyStateView(model: model2)
+            .frame(maxWidth: 360)
+    }
+    .frame(width: 600, height: 600)
+    .background(Color.figmaBackground)
 }
