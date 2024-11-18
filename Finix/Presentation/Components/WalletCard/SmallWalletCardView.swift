385a7968f7323c7fdb7a9f8916f0745c07cd69d9
Valerie <valerypavlikov@yandex.ru>
2024-11-18 12:14:32 +0300

    - add State to SmallWalletCardView - add ViewModel

--- Finix/Presentation/Components/WalletCard/SmallWalletCardView.swift
+++ Finix/Presentation/Components/WalletCard/SmallWalletCardView.swift
@@ -3,11 +3,36 @@
 import SwiftUI
 
 extension SmallWalletCardView {
-    struct Model {
+    class ViewModel: ObservableObject {
+        enum State {
+            case plain
+            case selected
+        }
+        
         let walletName: String
         let walletBalance: String
         let styleColor: Color
         let currencyIsoCode: String
+        
+        var backgroundColor: Color {
+            switch state {
+            case .plain:
+                Color.textWhite
+            case .selected:
+                styleColor
+            }
+        }
+        
+        var textColor: Color {
+            switch state {
+            case .plain:
+                Color.textSecondary
+            case .selected:
+                Color.textWhite
+            }
+        }
+        
+        @Published var state: State = .plain
         
         init(
             walletName: String = "",
@@ -24,95 +49,111 @@ extension SmallWalletCardView {
 }
 
 struct SmallWalletCardView: View {
-    let model: Model
+    @StateObject var viewModel: ViewModel
     
     var body: some View {
         VStack(alignment: .leading, spacing: 0) {
-            Group {
-                Text(model.walletName)
-                    .padding(.top, 8)
-                Text(model.walletBalance)
-                    .padding(.top, 4)
-            }
-            .padding(.leading, 8)
-            .font(.microTextMedium)
-            .foregroundStyle(Color.textSecondary)
-            HStack {
-                Spacer()
-                Text(model.currencyIsoCode)
-                    .font(.buttonMedium)
-                    .foregroundStyle(model.styleColor)
-                    .padding(.trailing, 5)
-                    .padding(.bottom, 1)
-//                Image("check-circle")
-//                    .resizable()
-//                    .aspectRatio(contentMode: .fit)
-//                    .frame(width: 30)
-//                    .offset(x: 3, y: 1)
-                
-            }
-            .frame(height: 30)
+            topLayer
+            bottomLayer
         }
         .walletCardFrame(
             height: 60,
-            backgroundColor: Color.textWhite,
+            backgroundColor: viewModel.backgroundColor,
             cornerRadius: 10,
-            strokeColor: model.styleColor
+            strokeColor: viewModel.styleColor
         )
+        .onTapGesture {
+            withAnimation {
+                viewModel.state = viewModel.state == .plain ? .selected : .plain
+            }
+        }
+    }
+    
+    var topLayer: some View {
+        Group {
+            Text(viewModel.walletName)
+                .padding(.top, 8)
+            Text(viewModel.walletBalance)
+                .padding(.top, 4)
+        }
+        .padding(.leading, 8)
+        .font(.microTextMedium)
+        .foregroundStyle(viewModel.textColor)
+    }
+    
+    var bottomLayer: some View {
+        HStack {
+            Spacer()
+            if viewModel.state == .plain {
+                Text(viewModel.currencyIsoCode)
+                    .font(.buttonMedium)
+                    .foregroundStyle(viewModel.styleColor)
+                    .padding(.trailing, 5)
+                    .padding(.bottom, 1)
+            } else {
+                Image("check-circle")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 30)
+                    .foregroundStyle(Color.textWhite)
+                    .offset(x: 3, y: 1)
+            }
+        }
+        .frame(height: 30)
     }
 }
 
 #Preview {
-    let modelUsd = SmallWalletCardView.Model(
+    let modelUsd = SmallWalletCardView.ViewModel(
         walletName: "Cash",
         walletBalance: "$750",
         styleColor: Color.customPrimary,
         currencyIsoCode: "USD"
     )
-    let modelUsdEmpty = SmallWalletCardView.Model(
+    let modelUsdEmpty = SmallWalletCardView.ViewModel(
         styleColor: Color.customPrimary,
         currencyIsoCode: "USD"
     )
     
-    let modelEur = SmallWalletCardView.Model(
+    let modelEur = SmallWalletCardView.ViewModel(
         walletName: "NeoBank",
         walletBalance: "€1 560",
         styleColor: Color.ocean,
         currencyIsoCode: "EUR"
     )
-    let modelEurEmpty = SmallWalletCardView.Model(
+    let modelEurEmpty = SmallWalletCardView.ViewModel(
         styleColor: Color.ocean,
         currencyIsoCode: "EUR"
     )
     
-    let modelRub = SmallWalletCardView.Model(
+    let modelRub = SmallWalletCardView.ViewModel(
         walletName: "Favourite Card",
         walletBalance: "₽350 059 865",
         styleColor: Color.rose,
         currencyIsoCode: "RUB"
     )
-    let modelRubEmpty = SmallWalletCardView.Model(
+    let modelRubEmpty = SmallWalletCardView.ViewModel(
         styleColor: Color.rose,
         currencyIsoCode: "RUB"
     )
     
     VStack {
         HStack {
-            SmallWalletCardView(model: modelUsd)
+            SmallWalletCardView(viewModel: modelUsd)
                 .frame(width: 90)
-            SmallWalletCardView(model: modelUsdEmpty)
-                .frame(width: 90)
-        }
-        HStack {
-            SmallWalletCardView(model: modelEur)
-                .frame(width: 90)
-            SmallWalletCardView(model: modelEurEmpty)
+            SmallWalletCardView(viewModel: modelUsdEmpty)
                 .frame(width: 90)
         }
         HStack {
-            SmallWalletCardView(model: modelRub)
+            SmallWalletCardView(viewModel: modelEur)
                 .frame(width: 90)
-            SmallWalletCardView(model: modelRubEmpty)
+            SmallWalletCardView(viewModel: modelEurEmpty)
+                .frame(width: 90)
+        }
+        HStack {
+            SmallWalletCardView(viewModel: modelRub)
+                .frame(width: 90)
+            SmallWalletCardView(viewModel: modelRubEmpty)
                 .frame(width: 90)
         }
     }
