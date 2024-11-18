1ec0035be948071d0717468fca2f1502344ccf8f
Valerie <valerypavlikov@yandex.ru>
2024-11-18 18:28:05 +0300

    - EmptyStateView refactoring

--- Finix/Presentation/Components/EmptyStates/EmptyStateView.swift
+++ Finix/Presentation/Components/EmptyStates/EmptyStateView.swift
@@ -10,42 +10,50 @@ extension EmptyStateView {
         }
         
         enum State {
-            case emptyTransaction
-            case emptyLimit
+            case upcomingTransaction
+            case recentTransaction
+            case limit
         }
         
         var state: State
+        let btnAction: () -> Void
         
         var imageName: String {
             switch state {
-            case .emptyTransaction:
-                return "calendar"
-            case .emptyLimit:
-                return "goal"
+            case .upcomingTransaction: "shopping-bag"
+            case .recentTransaction: "calendar"
+            case .limit: "target"
             }
         }
         
         var text: String {
             switch state {
-            case .emptyTransaction:
-                String(localized: "emptyTransactionText")
-            case .emptyLimit:
+            case .upcomingTransaction:
+                String(localized: "emptyUpcomingTransactionText")
+            case .recentTransaction:
+                String(localized: "emptyRecentTransactionText")
+            case .limit:
                 String(localized: "emptyLimitText")
             }
         }
         
         var buttonConfig: ButtonConfiguration {
             switch state {
-            case .emptyTransaction:
+            case .upcomingTransaction:
                 return .init(
-                    text: String(localized: "addTransactionBtnText")) {
-                        // Add logic here
-                    }
-            case .emptyLimit:
+                    text: String(localized: "addTransactionBtnText"),
+                    action: btnAction
+                )
+            case .recentTransaction:
                 return .init(
-                    text: String(localized: "setLimitBtnText")) {
-                        // Add logic here
-                    }
+                    text: String(localized: "addTransactionBtnText"),
+                    action: btnAction
+                )
+            case .limit:
+                return .init(
+                    text: String(localized: "setLimitBtnText"),
+                    action: btnAction
+                )
             }
         }
     }
@@ -55,64 +63,66 @@ struct EmptyStateView: View {
     let model: Model
     
     var body: some View {
-        
-        
-//        HStack {
-//            Image(model.imageName)
-//                .resizable()
-//                .aspectRatio(contentMode: .fit)
-//                .frame(height: 105)
-//                .padding(.trailing, 30)
-//            VStack(alignment: .leading) {
-//                Text(model.text)
-//                    .font(.smallTextTallRegular)
-//                    .foregroundStyle(Color.textSecondary)
-//                    .padding(.bottom, 22)
-//                Button {
-//                    model.buttonConfig.action()
-//                } label: {
-//                    HStack(alignment: .center) {
-//                        Text(model.buttonConfig.text)
-//                            .font(.bodyTextNarrowRegular)
-//                            .foregroundStyle(Color.customPrimary)
-//                    }
-//                    .padding(.horizontal, 16)
-//                    .padding(.vertical, 8)
-//                    .frame(maxWidth: .infinity)
-//                    .cornerRadius(10)
-//                    .overlay(
-//                      RoundedRectangle(cornerRadius: 10)
-//                        .inset(by: 0.5)
-//                        .stroke(Color.customPrimary)
-//                    )
-//                }
-//            }
-//            .frame(maxWidth: UIScreen.main.bounds.width * 0.5)
-//        }
-//        .padding(16)
-//        .frame(minHeight: 152)
-//        .background(Color.white)
-//        .cornerRadius(12)
-//        .overlay(
-//          RoundedRectangle(cornerRadius: 12)
-//            .inset(by: 0.25)
-//            .stroke(Color.borderStroke, lineWidth: 0.5)
-//        )
+        HStack {
+            Image(model.imageName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 120, height: 80)
+            VStack(alignment: .leading) {
+                Text(model.text)
+                    .font(.smallTextTallRegular)
+                    .foregroundStyle(Color.textSecondary)
+                    .padding(.bottom, 8)
+                Button {
+                    model.buttonConfig.action()
+                } label: {
+                    HStack(alignment: .center) {
+                        Text(model.buttonConfig.text)
+                            .font(.bodyTextNarrowRegular)
+                            .foregroundStyle(Color.customPrimary)
+                    }
+                    .padding(.vertical, 8)
+                    .frame(maxWidth: .infinity)
+                    .cornerRadius(10)
+                    .overlay(
+                      RoundedRectangle(cornerRadius: 10)
+                        .inset(by: 0.5)
+                        .stroke(Color.customPrimary)
+                    )
+                }
+            }
+            .padding(.horizontal, 16)
+            Spacer()
+        }
+        .walletCardFrame(
+            height: 125,
+            backgroundColor: Color.foreground,
+            cornerRadius: 12,
+            strokeColor: Color.borderStroke
+        )
     }
 }
 
 #Preview {
     let model1: EmptyStateView.Model = EmptyStateView.Model(
-        state: .emptyTransaction
-    )
+        state: .upcomingTransaction) {
+            print("Empty State of Upcoming Transaction")
+        }
     let model2: EmptyStateView.Model = EmptyStateView.Model(
-        state: .emptyLimit
-    )
+        state: .recentTransaction) {
+            print("Empty State of Recent Transaction")
+        }
+    let model3: EmptyStateView.Model = EmptyStateView.Model(
+        state: .limit) {
+            print("Empty State of Limit")
+        }
     
     VStack {
         EmptyStateView(model: model1)
             .frame(maxWidth: 360)
         EmptyStateView(model: model2)
+            .frame(maxWidth: 360)
+        EmptyStateView(model: model3)
             .frame(maxWidth: 360)
     }
     .frame(width: 600, height: 600)
