addd78ffca319b8a8058870e2445f4603f1149b6
Valerie <valerypavlikov@yandex.ru>
2024-11-18 12:45:17 +0300

    - localization refactoring

--- Finix/Presentation/Components/WalletCard/ActionMediumWalletCardView.swift
+++ Finix/Presentation/Components/WalletCard/ActionMediumWalletCardView.swift
@@ -2,25 +2,30 @@
 
 import SwiftUI
 
-struct ActionSmallWalletCardView: View {
+struct ActionMediumWalletCardView: View {
     let model: ActionWalletCardModel
     
     var body: some View {
-        VStack {
+        VStack(alignment: .leading) {
+            Text(model.actionName)
+                .font(.helperTextMedium)
+                .foregroundStyle(Color.textSecondary)
+                .padding([.top, .leading], 10)
+            Spacer()
             HStack {
                 Spacer()
                 Image(model.iconName)
                     .resizable()
                     .aspectRatio(contentMode: .fill)
-                    .frame(width: 30, height: 30)
+                    .frame(width: 40, height: 40)
                     .foregroundStyle(Color.customPrimary)
-                Spacer()
+                    .offset(x: 7, y: 5)
             }
         }
         .walletCardFrame(
-            height: 60,
+            height: 88,
             backgroundColor: Color.foreground,
-            cornerRadius: 10,
+            cornerRadius: 12,
             strokeColor: Color.borderStroke
         )
     }
@@ -35,10 +40,10 @@ struct ActionSmallWalletCardView: View {
     )
     
     VStack {
-        ActionSmallWalletCardView(model: addAccountModel)
-            .frame(width: 90)
-        ActionSmallWalletCardView(model: seeAllModel)
-            .frame(width: 90)
+        ActionMediumWalletCardView(model: addAccountModel)
+            .frame(width: 132)
+        ActionMediumWalletCardView(model: seeAllModel)
+            .frame(width: 132)
     }
     .frame(width: 800, height: 800)
     .background(Color.finixBackground)
