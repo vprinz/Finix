72c74c84f61484d7b65d9403aa3b15b98f95b674
Valerie <valerypavlikov@yandex.ru>
2024-11-21 10:29:14 +0300

    - add montlyBudget as optional

--- Finix/Presentation/Components/BudgetWidget/ExpenseSummaryView.swift
+++ Finix/Presentation/Components/BudgetWidget/ExpenseSummaryView.swift
@@ -5,8 +5,18 @@ import SwiftUI
 extension ExpenseSummaryView {
     struct Model {
         let expensesAmountWithCurrency: String
-        let monthlyBudgetAmountWithCurrency: String
+        let monthlyBudgetAmountWithCurrency: String?
         let chartSegmentModel: ChartSegmentView.Model
+        
+        init(
+            expensesAmountWithCurrency: String,
+            monthlyBudgetAmountWithCurrency: String? = nil,
+            chartSegmentModel: ChartSegmentView.Model
+        ) {
+            self.expensesAmountWithCurrency = expensesAmountWithCurrency
+            self.monthlyBudgetAmountWithCurrency = monthlyBudgetAmountWithCurrency
+            self.chartSegmentModel = chartSegmentModel
+        }
     }
 }
 
@@ -16,25 +26,9 @@ struct ExpenseSummaryView: View {
     var body: some View {
         VStack(spacing: 0) {
             HStack {
-                VStack(alignment: .leading) {
-                    Text("expensesText")
-                        .font(.captionNarrowRegular)
-                        .foregroundStyle(Color.textSecondary)
-                        .padding(.bottom, 8)
-                    Text(model.expensesAmountWithCurrency)
-                        .font(.buttonMedium)
-                        .foregroundStyle(Color.textPrimary)
-                }
+                expenses
                 Spacer()
-                VStack(alignment: .trailing) {
-                    Text("monthlyBudgetText")
-                        .font(.captionNarrowRegular)
-                        .foregroundStyle(Color.textSecondary)
-                        .padding(.bottom, 8)
-                    Text(model.monthlyBudgetAmountWithCurrency)
-                        .font(.buttonMedium)
-                        .foregroundStyle(Color.textPrimary)
-                }
+                monthlyBudget
             }
             .padding(.vertical, 16)
             ChartSegmentView(model: model.chartSegmentModel)
@@ -43,10 +37,36 @@ struct ExpenseSummaryView: View {
         .padding(.horizontal, 20)
         .cardFrame()
     }
+    
+    var expenses: some View {
+        VStack(alignment: .leading) {
+            Text("expensesText")
+                .font(.captionNarrowRegular)
+                .foregroundStyle(Color.textSecondary)
+                .padding(.bottom, 8)
+            Text(model.expensesAmountWithCurrency)
+                .font(.buttonMedium)
+                .foregroundStyle(Color.textPrimary)
+        }
+    }
+    
+    var monthlyBudget: some View {
+        model.monthlyBudgetAmountWithCurrency.map { monthlyBudget in
+            VStack(alignment: .trailing) {
+                Text("monthlyBudgetText")
+                    .font(.captionNarrowRegular)
+                    .foregroundStyle(Color.textSecondary)
+                    .padding(.bottom, 8)
+                Text(monthlyBudget)
+                    .font(.buttonMedium)
+                    .foregroundStyle(Color.textPrimary)
+            }
+        }
+    }
 }
 
 #Preview {
-    let chartSegmentModel: ChartSegmentView.Model = ChartSegmentView.Model(segments: [
+    let chartSegmentModel = ChartSegmentView.Model(segments: [
         ChartSegmentView.Model.SegmentItem(
             percentage: 0.2, color: Color.lavender
         ),
@@ -61,13 +81,37 @@ struct ExpenseSummaryView: View {
         )
     ])
     
-    let model: ExpenseSummaryView.Model = ExpenseSummaryView.Model(
+    let chartSegmentModelFull = ChartSegmentView.Model(segments: [
+        ChartSegmentView.Model.SegmentItem(
+            percentage: 0.3, color: Color.lavender
+        ),
+        ChartSegmentView.Model.SegmentItem(
+            percentage: 0.2, color: Color.turquoise
+        ),
+        ChartSegmentView.Model.SegmentItem(
+            percentage: 0.35, color: Color.lemon
+        ),
+        ChartSegmentView.Model.SegmentItem(
+            percentage: 0.15, color: Color.pink
+        )
+    ])
+    
+    let modelWithBudget = ExpenseSummaryView.Model(
         expensesAmountWithCurrency: "$2 750.67",
-        monthlyBudgetAmountWithCurrency: "$3 550.00", chartSegmentModel: chartSegmentModel
+        monthlyBudgetAmountWithCurrency: "$3 550.00",
+        chartSegmentModel: chartSegmentModel
     )
     
-    HStack {
-        ExpenseSummaryView(model: model)
+    let modelWithoutBudget = ExpenseSummaryView.Model(
+        expensesAmountWithCurrency: "$2 750.67",
+        chartSegmentModel: chartSegmentModelFull
+    )
+    
+    VStack {
+        ExpenseSummaryView(model: modelWithBudget)
+        .frame(width: 361)
+        
+        ExpenseSummaryView(model: modelWithoutBudget)
         .frame(width: 361)
     }
     .frame(width: 600, height: 600)
