c42c5657a0a9b503a0e9c95ee66e31448eaaa188
Valerie <valerypavlikov@yandex.ru>
2024-11-29 15:54:20 +0300

    - create CheckboxButtonView

---
+++ Finix/Presentation/Components/Controls/CheckboxButtonView.swift
@@ -0,0 +1,83 @@
+// Created by Valerie N. Prinz on 27/11/2024.
+
+import SwiftUI
+
+extension CheckboxButtonView {
+    struct Model {
+        let selected: Bool
+        let disabled: Bool
+        
+        var fillColor: Color {
+            selected ?
+                .customPrimary.opacity(disabledOpacity) :
+                .clear
+        }
+        
+        var strokeColor: Color {
+            if disabled {
+                return selected ?
+                    .clear :
+                    .finixGray.opacity(disabledOpacity)
+            }
+            return selected ? .customPrimary.opacity(disabledOpacity) : .finixGray.opacity(disabledOpacity)
+        }
+        
+        var overlayOpacity: Double {
+            selected ? 1 : 0
+        }
+        
+        private var disabledOpacity: Double {
+            disabled ? 0.6 : 1
+        }
+        
+        init(
+            selected: Bool = false,
+            disabled: Bool = false
+        ) {
+            self.selected = selected
+            self.disabled = disabled
+        }
+    }
+}
+
+struct CheckboxButtonView: View {
+    let model: Model
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 4)
+            .stroke(model.strokeColor, lineWidth: 2)
+            .frame(width: 22, height: 22)
+            .background(
+                RoundedRectangle(cornerRadius: 4)
+                    .fill(model.fillColor)
+            )
+            .overlay(
+                Image("check-mark")
+                    .foregroundColor(.white)
+                    .opacity(model.overlayOpacity)
+            )
+    }
+}
+
+#Preview {
+    let modelNotSelected: CheckboxButtonView.Model = .init()
+    let modelSelected: CheckboxButtonView.Model = .init(
+        selected: true
+    )
+    let modelDisabled: CheckboxButtonView.Model = .init(
+        disabled: true
+    )
+    let modelSelectedDisabled: CheckboxButtonView.Model = .init(
+        selected: true,
+        disabled: true
+    )
+    
+    HStack(spacing: 42) {
+        CheckboxButtonView(model: modelNotSelected)
+        CheckboxButtonView(model: modelSelected)
+        CheckboxButtonView(model: modelDisabled)
+        CheckboxButtonView(model: modelSelectedDisabled)
+    }
+    .padding()
+    .background(Color.finixBackground)
+}
