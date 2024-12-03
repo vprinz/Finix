15519739b4e503368a1ef4319c29b4aecd570e18
Valerie <valerypavlikov@yandex.ru>
2024-12-03 17:52:03 +0300

    - create Category model - create CategoryService - add Category icons

---
+++ Finix/Entity/Category.swift
@@ -0,0 +1,26 @@
+// Created by Valerie N. Prinz on 03/12/2024.
+
+import SwiftData
+import SwiftUI
+
+@Model
+final class Category {
+    @Attribute(.unique) var id: UUID
+    var name: String
+    var iconName: String
+    var iconColor: Color
+    var isDefault: Bool
+    
+    init(
+        id: UUID = UUID(),
+        name: String,
+        iconName: String,
+        iconColor: Color,
+        isDefault: Bool = false
+    ) {
+        self.id = id
+        self.name = name
+        self.iconName = iconName
+        self.iconColor = iconColor
+    }
+}
